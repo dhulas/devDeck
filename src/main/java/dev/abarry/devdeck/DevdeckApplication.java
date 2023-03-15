a12ba249186654d329b2e82e7a35039a67d73226
Abdoul <supadhulas@gmail.com>
2023-03-15 21:53:55 +0000

    initial commit

---
+++ src/main/java/dev/abarry/devdeck/DevdeckApplication.java
@@ -0,0 +1,13 @@
+package dev.abarry.devdeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/abarry/devdeck/DevdeckApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abarry.devdeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
